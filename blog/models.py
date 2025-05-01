f8c84ac364f5548e439079153e33ed2b3db1d96c
Konrad Pawlowski <konrad@Konrads-Air.lan>
2025-05-01 14:47:21 +0200

    Created List Profiles Page

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+    date_modified = DateTimeField(User, auto_now=True)
 
     def __str__(self):
         return self.user.username
+
+
