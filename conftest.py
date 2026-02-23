66b02139347940ab6787b59e020191ebf2bf23ca
Konrad Pawlowski <konradzapasowy879@gmail.com>
2026-02-23 20:28:01 +0100

    Tested post likes

--- conftest.py
+++ conftest.py
@@ -1,20 +1,20 @@
 import os
 import pytest
 import json
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 
 @pytest.fixture(scope="session")
-def test_data_fixture():
+def test_data():
     # read the file once for a whole session
-    with open("posts/tests/test_data.json", "r", encoding="utf-8") as file:
+    with open("posts/tests/test_data/test_data.json", "r", encoding="utf-8") as file:
         return json.load(file)
 
 @pytest.fixture
-def authenticated_page_fixture(page: Page, live_server, django_user_model, test_data_fixture):
-    test_username = test_data_fixture["user"]["username"]
-    test_password = test_data_fixture["user"]["password"]
+def authenticated_page(page: Page, live_server, django_user_model, test_data):
+    test_username = test_data["user"]["username"]
+    test_password = test_data["user"]["password"]
 
     # test user and database setup
 
@@ -31,4 +31,20 @@ def authenticated_page_fixture(page: Page, live_server, django_user_model, test_
     page.click("button[type='submit']")
 
     assert page.is_visible("text=Logout")
+    yield page
+
+@pytest.fixture(autouse=True)
+def isolate_media_root(settings, tmpdir):
+    # force django to use temporary folder in order to prevent duplicates appearing in the media folder
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+@pytest.fixture
+def published_post(test_data, authenticated_page):
+    # a prepared post to be tested - in order not to repeat the code
+    page = authenticated_page
+    test_blog_post_text = test_data["post"]["text"]
+    page.fill("#id_body", test_blog_post_text)
+    page.click("button[type='submit']")
+    expect(page.locator(".alert-dark").first).to_contain_text(test_blog_post_text)
+
     yield page
--- posts/tests/test_posts.py
+++ posts/tests/test_posts.py
@@ -1,15 +1,15 @@
 import pytest
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
 
 
 @pytest.mark.django_db(transaction=True)
-def test_user_can_create_post_with_text(live_server, authenticated_page_fixture, test_data_fixture):
+def test_user_can_create_post_with_text(authenticated_page, test_data):
     # receives a logged user from a fixture
-    page = authenticated_page_fixture
+    page = authenticated_page
     # finds a first post in the specified div
     posts = page.locator(".alert-dark")
     # creates a test post with text imported from test_data.json
-    test_blog_post_text = test_data_fixture["post"]["text"]
+    test_blog_post_text = test_data["post"]["text"]
     page.fill("#id_body", test_blog_post_text)
     page.click("button[type='submit']")
 
@@ -17,17 +17,27 @@ def test_user_can_create_post_with_text(live_server, authenticated_page_fixture,
 
 
 @pytest.mark.django_db(transaction=True)
-def test_user_can_create_post_with_text_and_image(live_server, authenticated_page_fixture, test_data_fixture):
+def test_user_can_create_post_with_text_and_image(authenticated_page, test_data):
     
-    page = authenticated_page_fixture
-    posts = page.locator(".alert-dark")
-    test_blog_post_text = test_data_fixture["post"]["text"]
-    page.set_input_files("input[type='file']", test_data_fixture["post"]["image_path"])
+    page = authenticated_page
+    # text and image path imported from 
+    
+    test_blog_post_text = test_data["post"]["text"]
+    page.set_input_files("input[type='file']", test_data["post"]["image_path"])
     page.fill("#id_body", test_blog_post_text)
-    page.click("button[type='submit']")
+    
     first_post = page.locator(".alert-dark").first
+    page.click("button[type='submit']")
 
     expect(first_post.get_by_role("img", name="Post image")).to_be_visible()
     expect(first_post).to_contain_text(test_blog_post_text)
     expect(first_post).to_contain_text("@test_user")
-    
+
+   
+@pytest.mark.django_db(transaction=True)
+def test_user_can_like_a_post(published_post):
+    page = published_post
+    first_post = page.locator(".alert-dark").first
+    first_post.locator(".fa.fa-heart-o").click()
+
+    expect(first_post.locator("#likes")).to_have_text("1")
